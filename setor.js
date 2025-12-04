00e3acf55b31cd48a48a31428f412d1631c2d1b2
PedroMachadord <134148347+PedroMachadord@users.noreply.github.com>
2025-12-04 16:29:40 -0300

    Add files via upload

---
+++ setor.js
@@ -0,0 +1,14 @@
+        // Dados completos dos setores
+        // Sequencial;Setor 
+        const setor = [
+            "1;Goiânia-Setor 1",
+            "2;Goiânia-Setor 2",
+            "3;Goiânia-Setor 3",
+            "4;Goiânia-Setor 4",
+            "5;Goiânia-Setor 5",
+            "6;Aparecida",
+            "7;Trindade",
+            "8;Senador Canedo",
+            "9;S L Montes Belos",
+            "10;Inhumas"
+        ];
