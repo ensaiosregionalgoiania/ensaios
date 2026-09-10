4a214e9e29fce20c6fea0ac4110bdf38f46df9f0
PedroMachadord <134148347+PedroMachadord@users.noreply.github.com>
2026-09-10 16:30:55 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -216,7 +216,7 @@
             "Residencial Boa Vista;Senador Canedo;2º DOMINGO;T;w3LGb9Mt2MG8fRRj9;8;Paulo Silva;Gesmar;T;-16.6753379;-49.1156796;DN 4N;",
             "Residencial Buritis;Senador Canedo;3ª SEXTA-FEIRA;N;xtNaHNaNA3XFWkpJ6;8;Jonatas Gonçalves;Gesmar;T;-16.690908;-49.0860527;DN 5N;",
             "Roselândia;Bela Vista;3º DOMINGO;T;a1rhog2ML8oLsnWW8;8;José Marcos;Rogério;T;-16.8632928;-49.0448407;DN 6N;",
-            "Santa Edwiges;Senador Canedo;2º DOMINGO;T;spZgKt2EzoCtY7tf9;8;Adair;Gesmar;T;-16.7074578;-49.1270861;DN 3N;",
+            "Santa Edwiges;Senador Canedo;3º DOMINGO;T;spZgKt2EzoCtY7tf9;8;Adair;Gesmar;T;-16.7074578;-49.1270861;DN 3N;",
             "Vila Mata Feia;Bela Vista;3ª TERÇA-FEIRA;N;LcpQV3qQMtYBWzk99;8;Vagmar Faleiro;Rogério;T;-16.8045837;-48.9050438;DN 6N;",
             "Vila Galvão;Senador Canedo;1ª SEXTA-FEIRA;N;mCD5gdLAp37sadDR6;8;Hernani Moraes (temporário);Gesmar;T;-16.6960791;-49.1850356;DN 5N;",
             "Jardim Primavera;S L Montes Belos;1º SáBADO;T;XB6yH7hTpZVmaKDs8;9;Wallison Klleuver;Jackson;T;-16.5113396;-50.3837027;DN 4N SN;",
