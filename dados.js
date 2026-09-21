589b204ac0211065ceca49e138acaebae4ca03d6
PedroMachadord <134148347+PedroMachadord@users.noreply.github.com>
2026-09-21 18:15:07 -0300

    Add files via upload

--- dados.js
+++ dados.js
@@ -248,7 +248,7 @@
             "Vila Yate/Vila Moraes;Goiânia;4º DOMINGO;T;TkdSEtZS7CChmnh47;1;Carlúcio;Ricardo Rosemberg;T;-16.6637406;-49.2239445;DN 3N 5N;24/05/2026",
             "Jardim flamboyant;Senador Canedo;2ª SEXTA-FEIRA;N;EHwJHJLdLy6zo3BL8;8;Arnaldo Osório;Gesmar;T;-16.7162344;-49.0988305;DN 5N;",
             "Vila Bandeirantes;Goiânia;3º DOMINGO;T;MycWjemojRqXnNgF7;1;TONIVALDO;HELTON RICARDO;T;-16.6713935;-49.2221748;DN 3N SN;",
-            "Residencial Juscelino Kubitschek(JK);Goiânia;5º DOMINGO;T1;WmQy2TizxLt65jyw6;4;;Jackson;T;-16.5846278;-49.3548674;DN 5N;",
+            "Residencial Juscelino Kubitschek(JK);Goiânia;5º DOMINGO;T1;WmQy2TizxLt65jyw6;4;Adriel Magalhães;Jackson;T;-16.5846278;-49.3548674;DN 5N;",
             "Claudinápois;Nazario;;N;VDEBWYQV31Nmd3d36;7;;;T;-16.5643753;-49.7728876;DN;",
             "Vila Perpetuo Socorro;Trindade;;N;GUrpFYdyU5m5UuMLA;7;;;T;-16.6587479;-49.4973899;2N SN;",
             "Solar Sao Francisco;Trindade;;N;AwcSY8Ks9BASumbi9;7;;;T;-16.5730941;-49.4172667;2N SN;",
